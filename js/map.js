b2ed6b3a58141f599faab12cf918d4d8b53a2129
skotperez <voraginebramante@gmail.com>
2016-03-26 10:34:33 +0100

    Makes base maps vars global and creates function to switch base map

--- js/map.js
+++ js/map.js
@@ -1,54 +1,61 @@
-// VARIABLES
-// de configuración inicial de la cartografía
+// Define configuración inicial de la cartografía
 var mapCenter = [43.3227, -1.9835]; // lat, lon
 var mapZoom = 14; // valores entre 1-19. Según la base cartográfica puede variar el rango
 
-// Carga el mapa
-function mapInit() {
-	// Base cartográfica
-	// 1. Mapbox
-	var mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
-	var mapboxAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
-	var mapboxBase = L.tileLayer(mapboxUrl, {
-		attribution: mapboxAttr,
-		maxZoom: 18,
-		id: 'skotperez.nkgo660p',
-		accessToken: 'pk.eyJ1Ijoic2tvdHBlcmV6IiwiYSI6ImNpZmVjMWd1bTAwZnp1Y2tuenhncHlvNHMifQ.yHojMKQz04-JIz6N4HyjyA'
-	});
-	var satelliteBase = L.tileLayer(mapboxUrl, {
-		attribution: mapboxAttr,
-		maxZoom: 18,
-		id: 'skotperez.nnf00875',
-		accessToken: 'pk.eyJ1Ijoic2tvdHBlcmV6IiwiYSI6ImNpZmVjMWd1bTAwZnp1Y2tuenhncHlvNHMifQ.yHojMKQz04-JIz6N4HyjyA'
-	});
-	// 2. OpenStreetMap
-	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttr = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osmBase = new L.tileLayer(osmUrl, {
-		attribution: osmAttr,
-		minZoom: 1,
-		maxZoom: 18,
-	});
+// Define contenedor para el mapa
+var map;
 
-	// Carga mapa en la variable map
-	var map = new L.map('map', {
+// Define bases cartográfica
+// 1. Mapbox
+var mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+var mapboxAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
+var mapboxBase = L.tileLayer(mapboxUrl, {
+	attribution: mapboxAttr,
+	maxZoom: 18,
+	id: 'skotperez.nkgo660p',
+	accessToken: 'pk.eyJ1Ijoic2tvdHBlcmV6IiwiYSI6ImNpZmVjMWd1bTAwZnp1Y2tuenhncHlvNHMifQ.yHojMKQz04-JIz6N4HyjyA'
+});
+var satelliteBase = L.tileLayer(mapboxUrl, {
+	attribution: mapboxAttr,
+	maxZoom: 18,
+	id: 'skotperez.nnf00875',
+	accessToken: 'pk.eyJ1Ijoic2tvdHBlcmV6IiwiYSI6ImNpZmVjMWd1bTAwZnp1Y2tuenhncHlvNHMifQ.yHojMKQz04-JIz6N4HyjyA'
+});
+// 2. OpenStreetMap
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttr = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osmBase = new L.tileLayer(osmUrl, {
+	attribution: osmAttr,
+	minZoom: 1,
+	maxZoom: 18,
+});
+
+// Define función para cargar el mapa
+function mapInit() {
+	map = new L.map('map', {
 		center: mapCenter,
 		zoom: mapZoom,
-		layers: [osmBase]
+		layers: [osmBase] // capas a mostrar inicialmente en el mapa
 	});
+}
 
-	// CONTROL: layer Switcher
+// Define función para añadir menú para elegir la base cartográfica a mostrar
+function baseMapSwitcher() {
 	var baseMaps = {
 		"OpenStreetMap": osmBase,
 		"Mapbox": mapboxBase,
 		"Mapbox Satélite": satelliteBase
 	};
 	L.control.layers(baseMaps).addTo(map);
-
 }
 
 // Cuando la página (document) haya cargado...
 $(document).ready(function() {
+
 	// ...muestra el mapa
 	mapInit();
+
+	// ...añade menú de bases cartográficas
+	baseMapSwitcher();
+
 });
