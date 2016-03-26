225557dfe89f04b18540e6f28da302d98ed61e78
skotperez <voraginebramante@gmail.com>
2016-03-26 09:21:49 +0100

    Basic files: jquery, leaflet librarys and script to load base map

---
+++ js/mapvars.js
@@ -0,0 +1,3 @@
+// Configuración inicial de la cartografía
+var mapCenter = [43.3227, -1.9835]; // lat, lon
+var mapZoom = 14; // valores entre 1-19. Según la base cartográfica puede variar el rango
